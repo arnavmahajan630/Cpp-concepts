bae5b2f60e4c826e86f7e6f9b0b4f1da5d15672e
OceanWhisperer <arnavmahajan630@gmail.com>
2025-08-03 18:01:36 +0530

    feat/learning-oops-and-beyond-C++

---
+++ overloading.hpp
@@ -0,0 +1,42 @@
+#include <cstring>
+class MyString  {
+    private:
+    char * str;
+    MyString(); // No args constructor
+    MyString(const char * s); // overloaded constructor
+    MyString(const MyString &source); // copy constructor
+    ~MyString();
+
+    
+};
+MyString::MyString():str(nullptr) {
+    str = new char[1];
+    str[0] = '\0';
+}
+
+MyString::MyString(const char * s):str(nullptr) {
+    if(s == nullptr) {
+        str = new char[1];
+        str[0] = '\0';
+    }
+    else {
+        str = new char[strlen(s) + 1];
+        strcpy(str, s);
+    }
+}
+
+MyString::MyString(const MyString &soruce):str(nullptr) {
+    if(soruce.str == nullptr) {
+        str = new char[1];
+        str[0] = '\0';
+    }
+    else {
+        str = new char [strlen(soruce.str) + 1];
+        strcpy(str, soruce.str);
+    }
+}
+
+MyString::~MyString() {
+    delete []str;
+}
+
