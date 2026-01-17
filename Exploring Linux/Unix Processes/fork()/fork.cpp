a71ab62c9d94a0410eb590c0d972e78644240105
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-01-17 12:25:27 +0530

    feat/mutliple-fork()-calls

--- Exploring Linux/Unix Processes/fork()/fork.cpp
+++ Exploring Linux/Unix Processes/fork()/fork.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 
 int main() {
-    printf("hello world\n");
-    int id = fork();
-    printf("hello to fork()! from: %d\n", id);
+    // printf("hello world\n");
+    // int id = fork();
+    // printf("hello to fork()! from: %d\n", id);
+
+
+    fork();
+    fork(); // 2^n processes are created reasoning is understood.
+    printf("hello from fork()\n");
 }
