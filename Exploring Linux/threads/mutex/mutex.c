a5eaa615735978f9c908f8678096d6d46995e68e
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-03-04 13:00:49 +0530

    mutexes in C

--- Exploring Linux/threads/mutex/mutex.c
+++ Exploring Linux/threads/mutex/mutex.c
@@ -2,6 +2,33 @@
 #include <unistd.h>
 #include <pthread.h>
 
+int count = 0;
+pthread_mutex_t mu; // create a mutex
+
+
+void * routine(void * ) {
+    for(int i=0; i < 1000000; ++i) {
+        pthread_mutex_lock(&mu); // lock the count variable.
+        count++;
+        pthread_mutex_unlock(&mu);
+    }
+}
+
 int main() {
+    pthread_t t1;
+    pthread_t t2;
+    pthread_mutex_init(&mu, NULL); // initialize the mutex
+    void *(* func_ptr)(void *) = routine;
+    pthread_create(&t1, NULL, func_ptr, NULL);
+    pthread_create(&t2, NULL, func_ptr, NULL);
     
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    // destroy the mutext
+    pthread_mutex_destroy(&mu);
+
+    // print result
+    printf("The final count is: %d", count);
+
 }
